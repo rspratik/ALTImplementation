a53680f7e535d651ce54f8c7b31a0aa2d4185f30
rspratik <pratikcs0063@gmail.com>
2017-01-19 09:21:31 -0500

    Updating Main.py
    Completed the functionality to retrieve all alt results.

--- Main.py
+++ Main.py
@@ -1,4 +1,4 @@
-print "--------------------------start--------------------------"
+print "-------------Starting Complete Alt implementation---------------"
 diction={}  
 d=1
 #Function to parse the bracketed problem statement and put the intermediate results(could be terminal or non terminal) into dictionary.
@@ -12,12 +12,12 @@ def parse(expression):
                 if not closeparen:
                     raise ValueError("Bad expression: Please check if you missed the parentheses")
                 diction["T"+str(d)] = result;
-                #print (result)
+                #print 'Debugging point In fn Parse #1 :'+''.join(result)
                 if 4 > len(result) > 0  :
                   result="T"+str(d)
                 d=d+1
                 items.append(result)
-                #print (result)
+                #print 'Debugging point In fn Parse #2 :'+(result)
             elif item == ')':
                 return items, True
             else:
@@ -34,59 +34,59 @@ problem_stmt="-(((p)&(-(q)))|((p)|(r)))"
 #problem_stmt="((p)|(q))"
 #problem_stmt="-((p)&(q))"
 #problem_stmt="(((p)|(q))|(r))"
-#problem_stmt="(((p)&(q))|((p)&(r)))|((q)&(r))"
+#problem_stmt="((((p)&(q))|((p)&(r)))|((q)&(r)))"
 #problem_stmt="(((p)&((q)|(r)))|((q)|(r)))"
 
 problem = parse(problem_stmt)
 
-#adding last/root element
-diction["T"+str(d)]=problem;
+#adding last/root element only if it's case of negation.
+if len(problem)==2:
+  diction["T"+str(d)]=problem;
 #print 'Debugging point#1 : '+str(d)
 
-# now performing flipping of operators over the and/or over the 3 length elements
 final_dict={}
-j=0;
+
 for i in range(len(diction)):
-  #print 'Debugging point#2 : '+str(i+1) +' = ' + ' '.join(diction["T"+str(i+1)])
-  j=j+1
   
-  #if len=3 that means involvement of binary operator
-  if len(diction["T"+str(i+1)])==3 :
-    #print 'Debugging point#3 : '+''.join(diction[diction["T"+str(i+1)][2]])
+  #single terminal handling
+  if len(diction["T"+str(i+1)])==1 :
+    final_dict["T"+str(i+1)] = diction["T"+str(i+1)]
     
-    # used join in following statement because a single element of dictionary is treated as list
-    a=''.join(diction[diction["T"+str(i+1)][0]])
-    b=''.join(diction[diction["T"+str(i+1)][2]])
-    #resetting the non-terminal symbols with terminal symbols
-    if len(a)>1:
-      diction["T"+str(i+1)][0]= '('+a+')'
-    else :
-      diction["T"+str(i+1)][0]= a
-    if len(b)>1:
-      diction["T"+str(i+1)][2]= '('+b+')'
-    else :
-      diction["T"+str(i+1)][2]= b
-    
-    final_dict[j]= ''.join(diction["T"+str(i+1)][0]) +'&'+''.join(diction["T"+str(i+1)][2])
-    j=j+1
-    final_dict[j]= ''.join(diction["T"+str(i+1)][0]) +'|'+''.join(diction["T"+str(i+1)][2])
-  
-  #if len=2 that means involvement of unary operator
+  #handling unary operator(negation) based expression
   elif len(diction["T"+str(i+1)])==2 :
-    #since here a would be an operator
-    a=''.join(diction["T"+str(i+1)][0])
-    #since here b would be a non terminal symbol
-    b=''.join(diction[diction["T"+str(i+1)][1]])
-    #resetting the non-terminal symbols with terminal symbols
-    diction["T"+str(i+1)][1]= '('+b+')'
-    final_dict[j]=a+'('+b+')'
+    # index =diction["T"+str(i+1)][1]
+    final_dict["T"+str(i+1)] = final_dict[diction["T"+str(i+1)][1]]; 
+    for l in range(len(final_dict["T"+str(i)])):
+      final_dict["T"+str(i+1)].append('-(' + final_dict["T"+str(i)][l] + ')')
+  
+  #handling binary operator ( & | )  based expression
+  elif len(diction["T"+str(i+1)])==3 :
+    temp_arr1 = final_dict[diction["T"+str(i+1)][0]]
+    temp_arr2 = final_dict[diction["T"+str(i+1)][2]]
+    final_dict["T"+str(i+1)] = temp_arr1 + temp_arr2
     
-  else :
-    if len(diction) !=i+1:
-      final_dict[j]= ''.join(diction["T"+str(i+1)])
-  #print str(j)+'Debugging point#4 : '+''.join(final_dict[j])
+    for x in range(len(temp_arr1)):
+      for y in range(len(temp_arr2)):
+        final_dict["T"+str(i+1)].append("("+temp_arr1[x] +'|'+ temp_arr2[y]+")")
+        final_dict["T"+str(i+1)].append("("+temp_arr1[x] +'&'+ temp_arr2[y]+")")
+    #resetting temp_arr1 & temp_arr2
+    temp_arr1=temp_arr2=[]
+  
+  #handling rest of expression which shd not be case
+  else:
+    print "Please check the dictionary elements as multiple elements encountered:(Should be less than 4)"
+
 print "Your problem statement : " + problem_stmt
 print "Your plausible ALT solutions are as followed : "
-for k in range(len(final_dict)):
-  print str(k+1) +' = ' + ''.join(final_dict[k+1])
+final_array=final_dict["T"+str(i+1)]
+
+#Removing repeated elements
+#final_array=list(set(final_array))
+#Sorting out array in ascending order of string length
+#final_array.sort(key = len)
+
+for k in range(len(final_array)):
+  print str(k+1) +' = ' + final_array[k]
+
+
 print "--------------------------end--------------------------"
